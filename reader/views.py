4e6acd6fd37e0286a266d232def670dd891553a5
Matt Behrens <askedrelic@gmail.com>
2010-03-07 17:22:13 -0500

    Added error handling for latest version of libgreader

--- reader/views.py
+++ reader/views.py
@@ -6,18 +6,21 @@ from lib.libgreader import Feed, GoogleReader
 def index(request):
     """Receives POST request with user/pass and displays that user's feeds"""
 
-    #verify username/pass and display error if bad
+    #validate username/pass and display error if bad
     username = request.POST['username']
     password = request.POST['password']
 
-    google = GoogleReader(username, password, 'FeederReader')
+    try:
+        google = GoogleReader(username, password, 'FeederReader')
+    except URLError:
+        #handle username/password error
+        pass
+
     if google.buildSubscriptionList():
         user_feed_list = google.getFeeds()
         
     templateLocation = 'reader/index.html'
     return direct_to_template(request, templateLocation, locals())
-
-
 
 
 #AJAX METHODS
