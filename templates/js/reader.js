88fbe7f307447ef208603a0732066575f061b88a
Matt Behrens <askedrelic@gmail.com>
2010-03-07 23:53:10 -0500

    Added drag/drop functionality and styling of drag boxes

--- templates/js/reader.js
+++ templates/js/reader.js
@@ -3,30 +3,20 @@
  *
  */
 $(function() {
+    $(".draggable").draggable({
+        cursorAt: { left: 100, top: 20 },
+        helper: function() {
+            return $(this).clone().css({
+                background: "blue",
+            });
+        }
+    });
+    $("#newfeedlist").droppable();
 
-    //setup data
-    var testdata = ["data1", "data2", "data3"];
-    $.get("/echo/", { echo: "echo data4"}, function(data) {
-        testdata.push(data);
-        testdata.push("data5");
-        });
-    var feedlist = $('div#userfeedlist ul');
-    feedlist.hide();
-
-    //load data
-    var testdata_length = testdata.length;
-    for(i=0; i<testdata_length; i++) {
-        feedlist.append("<li>" + testdata[i] + "</li>");
-        };
-
-    //display data
     $('#userfeedlist, #newfeedlist').css({'opacity':0});
-
     setTimeout(function() {
         $(".spinner").hide();
         $('#userfeedlist, #newfeedlist').css({'opacity':100});
-        feedlist.fadeIn(2000);
-        },2000);
-
-
+        $('div#userfeedlist ul').fadeIn(2000);
+        },1000);
 });
