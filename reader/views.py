cfaa551027bac52c91983c14d7b96ee6f71aa879
Matt Behrens <askedrelic@gmail.com>
2010-03-08 18:57:21 -0500

    Continued js drag/drop functionality.
    -Changed django to operate in debug mode right now.

--- reader/views.py
+++ reader/views.py
@@ -7,18 +7,25 @@ def index(request):
     """Receives POST request with user/pass and displays that user's feeds"""
 
     #validate username/pass and display error if bad
-    username = request.POST['username']
-    password = request.POST['password']
+    #username = request.POST['username']
+    #password = request.POST['password']
+    username = request.GET['username']
+    password = request.GET['password']
 
-    try:
-        google = GoogleReader(username, password, 'FeederReader')
-    except URLError:
-        #handle username/password error
-        #redirect to homepage with error?
-        pass
+    #try:
+    #    google = GoogleReader(username, password, 'FeederReader')
+    #except URLError:
+    #    #handle username/password error
+    #    #redirect to homepage with error?
+    #    pass
 
-    if google.buildSubscriptionList():
-        user_feed_list = google.getFeeds()
+    #if google.buildSubscriptionList():
+    #    user_feed_list = google.getFeeds()
+        
+    #DEBUG setup static user_feed_list
+    user_feed_list = []
+    for i in xrange(1,20):
+        user_feed_list.append(Feed("test%s"%i, "www.%s.com"%i, [i,i+1,i+2]))
         
     templateLocation = 'reader/index.html'
     return direct_to_template(request, templateLocation, locals())
--- templates/js/reader.js
+++ templates/js/reader.js
@@ -1,29 +1,60 @@
-/*
- * Reader data loading and GUI manipulation functions.
- *
- */
+var reader = function() {
+    //private data/functions
+    //
+    //public data/functions
+	return {
+        init : function() {
+            //setup #userfeedlist settings, toggles
+            //ordering
+        },
+        add_new_feed : function(new_feed) {
+            //swap DOM element
+            $("#newfeedlist ul").append(new_feed);
+            //remove drag
+            new_feed.draggable('disable');
+            //change styles
+        },
+        check_heights : function() {
+            //gaurantees the minimum height of both columns is
+            //equal
+
+
+        }
+	};
+}();
+
+//DOMReady events
 $(function() {
-    $(".draggable").draggable({
+    var user_feed_list = $("#userfeedlist ul li");
+    var new_feed_list = $("#newfeedlist");
+    
+    user_feed_list.draggable({
         cursorAt: { left: 80, top: 25 },
         helper: function() {
             return $(this).clone().css({
-                background: "blue",
+                'background':'#AAAAAA',
             });
-        }
+        },
+        opacity: 0.50,
+        revert: "invalid"
     });
-    $("#newfeedlist").droppable({
-        drop: function() {
-            $(this).html("Dropped!").css({
-                background: "yellow",
-                color: "black"
-            });
+    new_feed_list.droppable({
+        drop: function(event, ui) {
+            $(this).removeClass('ui-drophover');
+            reader.add_new_feed(ui.draggable);
+        },
+        over: function(event, ui) {
+            $(this).addClass('ui-drophover');
+        },
+        out: function(event, ui) {
+            $(this).removeClass('ui-drophover');
         }
     });
 
-    $('#userfeedlist, #newfeedlist').css({'opacity':0});
+    $(user_feed_list).hide();
+
     setTimeout(function() {
         $(".spinner").hide();
-        $('#userfeedlist, #newfeedlist').css({'opacity':100});
-        $('div#userfeedlist ul').fadeIn(2000);
+        $(user_feed_list).fadeIn(50);
         },500);
 });
