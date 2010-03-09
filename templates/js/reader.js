3fe6a37ccc2287c0e262b84fede8b78291adc581
Matt Behrens <askedrelic@gmail.com>
2010-03-08 22:35:32 -0500

    Changed the drag cursor to a "move" cursor. Also playing with colors. Bad idea.

--- templates/js/reader.js
+++ templates/js/reader.js
@@ -1,13 +1,17 @@
 var reader = function() {
     //private data/functions
-    //
+    var border_colors1 = ["#FFFF66","#FFCC00","#FF9900","#FF0000"]; 
+    var border_colors2 = ["#CE0000","#F7D708","#FF9E00","#009ECE"]; 
+    var border_colors3 = ["#FF9E00","#80FF00","#00FF00","#00FF80","#00FFFF","#0080FF","#0000FF","#8000FF"]; 
     //public data/functions
 	return {
-        init : function() {
+        init: function() {
             //setup #userfeedlist settings, toggles
-            //ordering
+            $.map($("#userfeedlist ul li"), function (el, index) {
+                return $(el).css("border-color", border_colors3[index % border_colors3.length]);
+            });
         },
-        add_new_feed : function(new_feed) {
+        add_new_feed: function(new_feed) {
             //move DOM element
             new_feed.appendTo("#newfeedlist ul");
             //remove drag
@@ -16,7 +20,7 @@ var reader = function() {
             new_feed.removeClass('ui-draggable');
             new_feed.addClass('ui-droppeditem');
         },
-        balance_heights : function() {
+        balance_heights: function() {
             //balance the height of both columns
             if($("#userfeedlist").height() >= $("#newfeedlist").height()) {
                 $("#newfeedlist").height($("#userfeedlist").height());
@@ -30,6 +34,8 @@ var reader = function() {
 //DOMReady events
 //Setup drag/drops and load elements
 $(function() {
+    reader.init();
+
     $("#userfeedlist ul li").draggable({
         cursorAt: { left: 80, top: 25 },
         helper: function() {
@@ -51,6 +57,8 @@ $(function() {
     $("#userfeedlist").hide();
     setTimeout(function() {
         $(".spinner").hide();
-        $("#userfeedlist").slideDown(200);
+        $("#userfeedlist").slideDown(200, function () {
+            reader.balance_heights();
+            });
         },500);
 });
