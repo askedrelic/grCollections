d2f6a4010d1d330edd561548522ae6dbf41aa6b9
Matt Behrens <askedrelic@gmail.com>
2010-03-09 00:55:12 -0500

    Moved media out of the templates dir

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROJECT_DIR = BASE_DIR
+PROJECT_DIR = BASE_DIR
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -26,7 +26,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates/")
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
