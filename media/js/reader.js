ccd622082da5e633334f49d35de14d718b71b235
Matt Behrens <askedrelic@gmail.com>
2010-03-10 22:31:14 -0500

    Added basic alphabetical JS sorting.

--- media/js/reader.js
+++ media/js/reader.js
@@ -1,8 +1,8 @@
 var reader = function() {
     //private data/functions
-    var border_colors1 = ["#FFFF66","#FFCC00","#FF9900","#FF0000"]; 
-    var border_colors2 = ["#CE0000","#F7D708","#FF9E00","#009ECE"]; 
-    var border_colors3 = ["#FF9E00","#80FF00","#00FF00","#00FF80","#00FFFF","#0080FF","#0000FF","#8000FF"]; 
+    var border_colors1 = ["#FFFF66","#FFCC00","#FF9900","#FF0000"];
+    var border_colors2 = ["#CE0000","#F7D708","#FF9E00","#009ECE"];
+    var border_colors3 = ["#FF9E00","#80FF00","#00FF00","#00FF80","#00FFFF","#0080FF","#0000FF","#8000FF"];
     //public data/functions
 	return {
         init: function() {
@@ -10,6 +10,8 @@ var reader = function() {
             $.map($("#userfeedlist ul li"), function (el, index) {
                 return $(el).css("border-color", border_colors3[index % border_colors3.length]);
             });
+
+            $('#userfeed_settings').append("<br/>Number of feeds: " + $('#userfeedlist ul li').length);
         },
         add_new_feed: function(new_feed) {
             //move DOM element
@@ -21,7 +23,7 @@ var reader = function() {
             new_feed.addClass('ui-droppeditem');
         },
         balance_heights: function() {
-            //balance the height of both columns, 
+            //balance the height of both columns,
             //for css effects and proper DOM div heights
             if($("#userfeedlist ul").height() >= $("#newfeedlist ul").height()) {
                 $("#newfeedlist").height($("#userfeedlist ul").height());
@@ -32,6 +34,19 @@ var reader = function() {
                 //background
                 $("#newfeedlist").height($("#newfeedlist ul").height() + 50);
             }
+        },
+        build_feed_item: function(title, url, categories) {
+        },
+        alpha_sort_feeds: function() {
+            var userlist = $('#userfeedlist ul');
+            var feeds = userlist.children('li').get();
+            feeds.sort(function(a, b) {
+                console.log(a, $(a));
+                var compA = $(a).text().toUpperCase();
+                var compB = $(b).text().toUpperCase();
+                return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+            })
+            $.each(feeds, function(idx, itm) { userlist.append(itm); });
         }
 	};
 }();
@@ -59,11 +74,16 @@ $(function() {
         activeClass: 'ui-drophover'
     });
 
-    //Remove the collection name input field text on click 
+    //Remove the collection name input field text on click
     $("#newfeedtext").bind('click', function() {
             this.value = '';
     });
 
+    //Remove the collection name input field text on click
+    $("#alpha_toggle").bind('click', function() {
+            reader.alpha_sort_feeds();
+            return false;
+    });
 
     $("#userfeedlist").hide();
     setTimeout(function() {
