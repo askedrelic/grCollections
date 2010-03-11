c14d92c007761057d840d3749e010e48fbfe6c08
Matt Behrens <askedrelic@gmail.com>
2010-03-11 14:41:37 -0500

    Feedlist is now returned alpha sorted with list of uniq categories

--- reader/views.py
+++ reader/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 
 from lib.libgreader import Feed, GoogleReader
 import lxml.etree
+import json
+
+def convert_to_builtin_type(obj):
+    # Convert objects to a dictionary of their representation
+    d = {}
+    d.update(obj.__dict__)
+    return d
 
 def index(request):
     """
     Receives POST request with GReader user/pass or OPML file of feeds.
-    Uses either to setup list of feeds.
+    Uses either to setup list of alpha sorted feeds.
     """
     if request.method == 'POST':
         if 'username' in request.POST and request.POST['username'] and\
@@ -32,7 +39,7 @@ def index(request):
             #error!
             pass
 
-    print [str(f) for f in user_feed_list]
+    #print json.dumps(user_feed_list,  sort_keys=True, indent=2, default=convert_to_builtin_type)
     templateLocation = 'reader/index.html'
     return direct_to_template(request, templateLocation, locals())
 
@@ -46,30 +53,51 @@ def getGoogleFeeds(username, password):
 
     if google.buildSubscriptionList():
         user_feed_list = google.getFeeds()
+
+    #add unique list of categories in 0 place
+    user_feed_list.insert(0, uniqifyCategories(user_feed_list))
     return user_feed_list
 
 def getOPMLFeeds(opmlfile):
-    #this only pulls 1 level deep rss feeds right now, should be made recursive
-    #returns a List of Feeds
+    """returns a List of Feeds"""
+    #TODO: this only pulls 1 level deep rss feeds right now, should be made recursive
     new_feeds = lxml.etree.fromstring(opmlfile)
-    feed_list = []
+    #all feeds using Feed.title as the key, value as the Feed obj
+    feed_list = {}
 
-    title = new_feeds.xpath("/opml/head/title")[0].text
-
+    #title = new_feeds.xpath("/opml/head/title")[0].text
     #all RSS feeds in a doc, with dups
     #all_feeds = new_feeds.xpath("/opml/body//*[@type]")
-    #top level categories
-    top_cats = new_feeds.xpath("/opml/body/outline[not(@type)]")
-    #top level rss feeds, without categories
-    uncat_feeds = new_feeds.xpath("/opml/body/outline[@type]")
 
-    for category in top_cats:
-        for el in category:
+    #add all top level categories
+    top_level_categories = new_feeds.xpath("/opml/body/outline[not(@type)]")
+    for individual_category in top_level_categories:
+        for el in individual_category:
             title = el.attrib['title']
             url = el.attrib['xmlUrl']
-            cat_list = [category.attrib['title']]
-            feed_list.append(Feed(title, url, cat_list))
+            feed_category = individual_category.attrib['title']
+            new_feed = Feed(title, url, [feed_category])
+            if title not in feed_list:
+                feed_list[title] = new_feed
+            #if does exist, add feed's categories
+            else:
+                feed_list[title].categories.append(feed_category)
 
+    #add all top level rss feeds, without categories
+    uncat_feeds = new_feeds.xpath("/opml/body/outline[@type]")
+    for feed in uncat_feeds:
+        title = feed.attrib['title']
+        url = feed.attrib['xmlUrl']
+        new_feed = Feed(title, url, [])
+        if title not in feed_list:
+            feed_list[title] = new_feed
+    
+    #convert to List and sort based off the Feed title
+    feed_list = feed_list.values()
+    feed_list.sort(key=lambda obj: obj.title.lower())
+
+    #add unique list of categories in 0 place
+    feed_list.insert(0, uniqifyCategories(feed_list))
     return feed_list
 
 def getOPMLTitle(opmlfile):
@@ -82,6 +110,13 @@ def getOPMLTitle(opmlfile):
     sub_index = title_list.index('subscriptions')
     title = ''.join(title_list[:sub_index])
     return title
+
+def uniqifyCategories(feedlist):
+    #find all categories in a list of feeds and return them as a single list
+    categories = set()
+    for feed in feedlist:
+        [categories.add(cat) for cat in feed.categories] 
+    return sorted(list(categories))
 
 #AJAX METHODS
 def echo(request):
@@ -96,4 +131,7 @@ def getDemoFeeds():
     feed_list.append(Feed('Superpoop', 'http://superpoop.com/rss/rss.php', ['comics']))
     feed_list.append(Feed('Explosm.net', 'http://feeds.feedburner.com/Explosm', ['comics']))
     feed_list.append(Feed('xkcd', 'http://xkcd.com/rss.xml', ['comics']))
+
+    #add unique list of categories in 0 place
+    feed_list.insert(0, uniqifyCategories(feed_list))
     return feed_list
